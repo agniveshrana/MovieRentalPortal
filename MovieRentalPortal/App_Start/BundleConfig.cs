14958237df9c556417ba82081aa86a8dbb2f8a1f
agniveshrana <agniveshrana@gmail.com>
2019-11-29 01:32:46 +0530

    Adding jquery.DataTable bundle to the project and implement it in customer-->index view page

--- MovieRentalPortal/App_Start/BundleConfig.cs
+++ MovieRentalPortal/App_Start/BundleConfig.cs
@@ -9,7 +9,9 @@ namespace MovieRentalPortal
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/DataTables/jquery.dataTables.js",
+                        "~/Scripts/DataTables/dataTables.bootstrap.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -26,6 +28,7 @@ namespace MovieRentalPortal
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap-lumen.css",
+                      "~/Content/DataTables/css/dataTables.bootstrap.css",
                       "~/Content/site.css"));
         }
     }
